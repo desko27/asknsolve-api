5c2f6d3ff428f136c248253c97683f12f1217f0c
desko27 <desko27@gmail.com>
2015-03-17 14:56:34 +0100

    phpmyadmin on vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -6,5 +6,6 @@ Vagrant.configure(2) do |config|
   config.vm.box = "hashicorp/precise32"
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.network :forwarded_port, host: 5000, guest: 5000
+  config.vm.network :forwarded_port, host: 8080, guest: 80
   
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,27 @@ apt-get update
 apt-get install -y python-pip python-mysqldb
 pip install iniparse sqlsoup flask hashids
 
+# other useful tools
+apt-get install -y curl
+
 # install mysql server
 apt-get install -y debconf-utils
-echo mysql-server mysql-server/root_password password 1234 | debconf-set-selections
-echo mysql-server mysql-server/root_password_again password 1234 | debconf-set-selections
+echo 'mysql-server mysql-server/root_password password 1234' | debconf-set-selections
+echo 'mysql-server mysql-server/root_password_again password 1234' | debconf-set-selections
 apt-get install -y mysql-server
+
+# install phpmyadmin
+apt-get install -y apache2
+echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password-confirm password 1234' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/admin-pass password 1234' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/app-pass password 1234' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
+apt-get install -y phpmyadmin
+echo "Include phpmyadmin.conf" >> /etc/apache2/apache2.conf
+ln -s /etc/phpmyadmin/apache.conf /etc/apache2/phpmyadmin.conf
+php5enmod mcrypt
+service apache2 restart
 
 # import database
 mysql -u root -p1234 < /vagrant/db/database.sql
